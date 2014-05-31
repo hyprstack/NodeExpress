6012784194351f31901d8353292ce07410037732
MMendes <mario.c.mendes1985@gmail.com>
2014-05-31 14:42:29 +0100

    UUpdate comments

--- server.js
+++ server.js
@@ -6,3 +6,11 @@ app.get('/', function(req, res) {
 });
 
 app.listen(3000);
+
+/*
+	Run this with Node.js in your command line by typing:
+		$ node server
+
+	To view the message open your browser at: 
+		HTTP://localhost:3000/
+*/
